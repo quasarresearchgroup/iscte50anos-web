0c9c25cd927b4b988de27d021ceee3268eac5768
JoaoAlmeida-dev <joao.ranger@gmail.com>
2022-09-21 14:20:25 +0100

    Scaling widgets up

--- lib/pages/timeline/timeline_tile.dart
+++ lib/pages/timeline/timeline_tile.dart
@@ -46,10 +46,8 @@ class _EventTimelineTileState extends State<EventTimelineTile> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedOpacity(
-      duration: const Duration(milliseconds: 300),
-      opacity: opacity,
-      child: InkWell(
+    return OrientationBuilder(builder: (context, orientation) {
+      return InkWell(
         splashColor: color2,
         highlightColor: color2,
         enableFeedback: true,
@@ -60,9 +58,9 @@ class _EventTimelineTileState extends State<EventTimelineTile> {
           });
           widget.handleEventSelection(widget.event.id);
           /* Navigator.pushNamed(
-            context,
-            "${TimeLineDetailsPage.pageRoute}/${widget.data.id}",
-          );*/
+              context,
+              "${TimeLineDetailsPage.pageRoute}/${widget.data.id}",
+            );*/
         },
         child: TimelineTile(
           beforeLineStyle: widget.lineStyle,
@@ -73,44 +71,61 @@ class _EventTimelineTileState extends State<EventTimelineTile> {
           isFirst: widget.isFirst,
           isLast: widget.isLast,
           indicatorStyle: IndicatorStyle(
-            width: 30,
-            height: 50,
+            width: MediaQuery.of(context).size.width * 0.08,
+            height: MediaQuery.of(context).size.width * 0.1,
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-            drawGap: true,
             indicator: Container(
               decoration: BoxDecoration(
-                  color: !widget.isEven
-                      ? Colors.transparent
-                      : Theme.of(context).primaryColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(20))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    widget.event.dateTime.monthName(),
-                    style: TextStyle(
-                      color: widget.isEven ? Colors.white : null,
+                color: !widget.isEven
+                    ? Colors.transparent
+                    : Theme.of(context).primaryColor,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          widget.event.dateTime.monthName(),
+                          style: TextStyle(
+                            color: widget.isEven ? Colors.white : null,
+                          ),
+                          textScaleFactor: 1,
+                          maxLines: 1,
+                        ),
+                      ),
                     ),
-                    textScaleFactor: 1,
-                    maxLines: 1,
-                  ),
-                  Text(
-                    widget.event.dateTime.day.toString(),
-                    style: TextStyle(
-                      color: widget.isEven ? Colors.white : null,
+                    Expanded(
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          widget.event.dateTime.day.toString(),
+                          style: TextStyle(
+                            color: widget.isEven ? Colors.white : null,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
           endChild: TimelineInformationChild(
               isEven: widget.isEven, data: widget.event),
-          startChild: Center(child: widget.event.scopeIcon),
+          startChild: Padding(
+            padding:
+                EdgeInsets.all(orientation == Orientation.landscape ? 20 : 0),
+            child: Center(child: widget.event.scopeIcon),
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
 
@@ -123,31 +138,39 @@ class TimelineInformationChild extends StatelessWidget {
 
   final bool isEven;
   final Event data;
+  final double padding = 20;
 
   @override
   Widget build(BuildContext context) {
     Color? textColor = !isEven ? Colors.white : null;
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(padding),
       child: Container(
         decoration: BoxDecoration(
-            color: isEven ? Colors.transparent : Theme.of(context).primaryColor,
-            borderRadius: const BorderRadius.all(Radius.circular(20))),
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Center(
-                    child: Text(data.title,
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          color: textColor,
-                        ))),
+          color: isEven ? Colors.transparent : Theme.of(context).primaryColor,
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(padding),
+                child: Text(
+                  data.title,
+                  maxLines: 3,
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    color: textColor,
+                  ),
+                ),
               ),
-              Column(
+            ),
+            Padding(
+              padding: EdgeInsets.all(padding),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Icon(
@@ -159,9 +182,9 @@ class TimelineInformationChild extends StatelessWidget {
                       //? const Icon(Icons.check, color: Colors.lightGreenAccent)
                       : Container(),
                 ],
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
